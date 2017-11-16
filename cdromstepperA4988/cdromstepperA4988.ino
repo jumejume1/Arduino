35719023185efbbca084bd281c68881039c909ed
jumejume1 <macbookair@MacbooKAirs-MacBook-Air.local>
2017-11-17 02:18:15 +0700

    Arduino source code

---
+++ cdromstepperA4988/cdromstepperA4988.ino
@@ -0,0 +1,37 @@
+// defines pins numbers
+const int stepPin = 2; 
+const int dirPin = 5; 
+const int enPin = 8;
+void setup() {
+  
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+
+  pinMode(enPin,OUTPUT);
+  digitalWrite(enPin,LOW);
+  
+}
+void loop() {
+  
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  delay(1000); // One second delay
+
+  digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+  
+}
